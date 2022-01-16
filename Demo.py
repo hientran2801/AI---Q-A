ce0060c1435ee6100fa8a2a6ce62e4adf0905b6d
hientran2801 <hientt@elcom.com.vn>
2022-01-16 23:27:40 +0700

    Task2 miss code crawl data

--- Demo.py
+++ Demo.py
@@ -1,9 +1,8 @@
-
 import pandas as pd
+import pymongo
 import numpy as np
 import string
 import pickle
-
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
@@ -11,7 +10,10 @@ from nltk import pos_tag
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy import sparse
-from pyvi import ViTokenizer, ViPosTagger
+from vncorenlp import VnCoreNLP
+from sklearn.svm import SVC
+from sklearn.metrics.pairwise import chi2_kernel
+
 
 def get_stopwords_list(stop_file_path):
     with open(stop_file_path, 'r', encoding="utf-8") as f:
@@ -22,27 +24,28 @@ def get_stopwords_list(stop_file_path):
 stopwords_path = 'input/vietnamese-stopwords'
 stopwords_list = get_stopwords_list(stopwords_path)
 
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+mydb = myclient['mydatabase']
+mycol = mydb["database"]
 
 
-
-all_data = pd.read_csv('input/questionanswer-dataset/db.txt', sep='\t')
-
-
+cursor = mycol.find()
+all_data = pd.DataFrame(list(cursor))
 all_data['AnswerA'] = all_data['AnswerA'] + '\n' + all_data['AnswerB'] + '\n' + all_data['AnswerC'] + '\n' + all_data['AnswerD']
 all_data = all_data[['Question', 'Answer', 'AnswerA']]
-
 all_data.dropna(axis=0)
-
 all_data.drop_duplicates(subset='Question')
 
-lemmatizer = WordNetLemmatizer()
 
+lemmatizer = WordNetLemmatizer()
+annotator = VnCoreNLP("<FULL-PATH-to-VnCoreNLP-jar-file>", annotators="wseg,pos", max_heap_size='-Xmx2g')
 
 def my_tokenizer(doc):
-    words = ViTokenizer.tokenize(doc)
-
-    pos_tags = ViPosTagger.postagging(words)
-
+    # words = word_tokenize(doc)
+    # print(words)
+    # pos_tags = pos_tag(words)
+    # print(pos_tags)
+    pos_tags = annotator.annotate(doc)
     non_stopwords = [w for w in pos_tags if not w[0].lower() in stopwords_list]
     non_punctuation = [w for w in non_stopwords if not w[0] in string.punctuation]
 
@@ -60,26 +63,23 @@ def my_tokenizer(doc):
             pos = wordnet.NOUN
 
         lemmas.append(lemmatizer.lemmatize(w[0], pos))
-
     return lemmas
 
 
-tfidf_vectorizer = TfidfVectorizer(tokenizer=my_tokenizer)
-# print(str(tfidf_vectorizer))
-tfidf_matrix = tfidf_vectorizer.fit_transform(tuple(all_data['Question']))
-# print(tfidf_matrix)
+# tfidf_vectorizer = TfidfVectorizer(tokenizer=my_tokenizer)
+# tfidf_matrix = tfidf_vectorizer.fit_transform(tuple(all_data['Question']))
+# pickle.dump(tfidf_vectorizer, open("crawlData/myvectorizer.pickle", "wb"))
+# sparse.save_npz("crawlData/mymatrix.npz", tfidf_matrix)
 
-
-
-
-# sparse.save_npz("yourmatrix.npz", tfidf_matrix)
-# pickle.dump(tfidf_vectorizer, open("yourvectorizer.pickle", "wb"))
-# tfidf_vectorizer = pickle.load(open("yourvectorizer.pickle", "rb"))
-# tfidf_matrix = sparse.load_npz("yourmatrix.npz")
+tfidf_vectorizer = pickle.load(open("crawlData/myvectorizer.pickle", "rb"))
+tfidf_matrix = sparse.load_npz("crawlData/mymatrix.npz")
 
 def ask_question(question):
     query_vect = tfidf_vectorizer.transform([question])
     similarity = cosine_similarity(query_vect, tfidf_matrix)
+    # print(query_vect)
+    # similarity = SVC(kernel=chi2_kernel).fit(tfidf_matrix, query_vect)
+    # similarity.predict
     max_similarity = np.argmax(similarity, axis=None)
 
     if (np.amax(similarity, axis=None) > 0.4):
